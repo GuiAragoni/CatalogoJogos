561e8b70d5ba4ec4c9484bd818b1cb92ed5f2ba1
Gui Aragoni <69279925+GuiAragoni@users.noreply.github.com>
2021-07-16 00:12:34 -0300

    Projeto Dio

---
+++ Catalogo/Catalogo/Exceptions/JogoNaoCadastradoException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalogo.Exceptions
+{
+    public class JogoNaoCadastradoException : Exception
+    {
+        public JogoNaoCadastradoException()
+            : base("Este jogo já foi cadastrado")
+        { }
+    }
+}
